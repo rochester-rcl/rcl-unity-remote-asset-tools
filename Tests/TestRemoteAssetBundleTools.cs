22aa7229418d7bc8a1e6adcfbe60808668baca2d
jromphf <jromphf@library.rochester.edu>
2019-06-04 14:54:07 -0400

    working on tests before writing GUI portion

---
+++ Tests/TestRemoteAssetBundleTools.cs
@@ -0,0 +1,16 @@
+namespace TestRemoteAssetBundleTools {
+    using UnityEngine.TestTools;
+    using NUnit.Framework;
+    class UnitTests {
+
+        [SetUp]
+        public void Setup() {
+
+        }
+
+        [TearDown]
+        public void TearDown() {
+
+        }
+    }
+}
